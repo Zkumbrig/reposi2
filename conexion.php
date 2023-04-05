bc74eca47404e4d1a04c8dacdf5cc0262b82bcb2
Zkumbrig <lveraperez6@gmai.com>
2023-04-05 16:11:58 -0500

    versionueva

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-38.railway.app";
-	$port = "port=8029";
+	$host = "host=containers-us-west-191.railway.app";
+	$port = "port=5624";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=JIcL9swq3Y5O3TS911yU";
+	$password = "password=xrtXMPoJ4Ch9HFeH1Bex";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
